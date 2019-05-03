ef444e5260e0f0376265608138e0bfce9b5e344d
Jose Tobar <josectobar@gmail.com>
2019-05-02 23:30:33 -0600

    minor fixes

--- server/AuthController.js
+++ server/AuthController.js
@@ -1,63 +1,69 @@
-const bcrypt = require('bcryptjs')
+const bcrypt = require("bcryptjs")
 
 module.exports = {
-    register: async (req, res) => {
-        try {
-            const db = req.app.get('db')
-            const { username, name, password, email } = req.body
+  register: async (req, res) => {
+    try {
+      const db = req.app.get("db")
+      const { username, name, password, email } = req.body
 
-            let authUser = await db.Auth.check_user({username})
-            authUser = authUser[0]
-            if (authUser) {
-                return res.status(409).send('user already exist')
-            }
-            const salt = bcrypt.genSaltSync(10)
-            const hash = bcrypt.hashSync(password, salt)
+      let authUser = await db.Auth.check_user({ username })
+      authUser = authUser[0]
+      if (authUser) {
+        return res.status(409).send("user already exist")
+      }
+      const salt = bcrypt.genSaltSync(10)
+      const hash = bcrypt.hashSync(password, salt)
 
-            const user = await db.Auth.register({name, username, email, password:hash})
-            req.session.user = user[0]
-            res.status(200).send(req.session.user)
-        } catch (err) {
-            console.log('error registering user', err)
-            res.sendStatus(500)
-        }
-    },
-    login: async (req, res) => {
-        try {
-            const db = req.app.get('db')
-            const { username, password }  = req.body
-            let authUser = await db.Auth.check_user({username})
-            authUser = authUser[0]
-            
-            if (!authUser) {
-                return res.status(401).send('user not found')
-            }
-            
-            const authPassword = bcrypt.compareSync(password, authUser.password)
-            
-            if (authPassword) {
-                delete authUser.password
-                req.session.user= authUser
-                res.status(200).send(req.session.user)
-            } else {
-                res.status(401).send('incorrect password')
-            }
-        } catch (err) {
-            console.log('error logining in', err)
-            res.sendStatus(500)
-        }
-    }, 
-    current: (req, res) => {
-        const { user } = req.session
-        if (user) {
-            res.status(200).send(user)
-        } else {
-            res.sendStatus(401)
-        }
-    },
-    logout: (req, res) => {
-        req.session.destroy(function(){
-            res.sendStatus(200)
-        })
+      const user = await db.Auth.register({
+        name,
+        username,
+        email,
+        password: hash
+      })
+      req.session.user = user[0]
+      res.status(200).send(req.session.user)
+    } catch (err) {
+      console.log("error registering user", err)
+      res.sendStatus(500)
     }
+  },
+  login: async (req, res) => {
+    try {
+      const db = req.app.get("db")
+      const { username, password } = req.body
+      let authUser = await db.Auth.check_user({ username })
+      authUser = authUser[0]
+
+      if (!authUser) {
+        return res.status(401).send("user not found")
+      }
+
+      const authPassword = bcrypt.compareSync(password, authUser.password)
+
+      if (authPassword) {
+        delete authUser.password
+        req.session.user = authUser
+        res.status(200).send(req.session.user)
+      } else {
+        res.status(401).send("incorrect password")
+      }
+    } catch (err) {
+      console.log("error logining in", err)
+      res.sendStatus(500)
+    }
+  },
+  current: (req, res) => {
+    const { user } = req.session
+    if (user) {
+      res.status(200).send(user)
+    } else {
+      res.sendStatus(401)
+    }
+  },
+  logout: (req, res) => {
+    console.log(req.session)
+    req.session.destroy(function() {
+      res.sendStatus(200)
+    })
+  }
 }
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,66 +1,65 @@
-import React, { Component } from "react";
-import { Link } from "react-router-dom";
-import { connect } from "react-redux";
-import { updateUser } from "../../ducks/reducer";
+import React, { Component } from "react"
+import { Link } from "react-router-dom"
+import { connect } from "react-redux"
+import { updateUser } from "../../ducks/reducer"
 import logo from "../../logo.png"
 
 //MaterialUI:
-import ButtonUI from "../Button/ButtonUI";
-import Paper from "@material-ui/core/Paper";
+import ButtonUI from "../Button/ButtonUI"
+import Paper from "@material-ui/core/Paper"
 
-import axios from "axios";
+import axios from "axios"
 
 class Login extends Component {
   constructor() {
-    super();
+    super()
     this.state = {
       username: ``,
       password: ``
-    };
+    }
   }
 
   componentDidMount() {
-    this.handleCurrent();
+    this.handleCurrent()
   }
 
   handleCurrent = async () => {
-    const { id } = this.props;
+    const { id } = this.props
     if (!id) {
       try {
-        let verifySession = await axios.get("/auth/current");
-        this.props.updateUser(verifySession.data);
-        this.handleWizardDisplay();
+        let verifySession = await axios.get("/auth/current")
+        this.props.updateUser(verifySession.data)
+        this.handleWizardDisplay()
       } catch (err) {}
     } else {
-      console.log(this.props);
-      this.props.history.push("/day/dashboard");
+      this.props.history.push("/day/dashboard")
     }
-  };
+  }
 
   handleWizardDisplay = () => {
     this.props.wizard
       ? this.props.history.push("/wizard/stepone")
-      : this.props.history.push("/day/dashboard");
-  };
+      : this.props.history.push("/day/dashboard")
+  }
 
   handleInput = e => {
-    const { name, value } = e.target;
+    const { name, value } = e.target
     this.setState({
       [name]: value
-    });
-  };
+    })
+  }
 
   handleLogin = async () => {
     try {
-      let postLogin = await axios.post("/auth/login", this.state);
-      this.props.updateUser(postLogin.data);
-      this.setState({ username: ``, password: `` });
-      this.handleWizardDisplay();
+      let postLogin = await axios.post("/auth/login", this.state)
+      this.props.updateUser(postLogin.data)
+      this.setState({ username: ``, password: `` })
+      this.handleWizardDisplay()
     } catch (err) {
-      console.log(err);
-      alert(`Incorrect username or password`);
+      console.log(err)
+      alert(`Incorrect username or password`)
     }
-  };
+  }
 
   render() {
     return (
@@ -85,30 +84,30 @@ class Login extends Component {
           />
           <ButtonUI
             action={this.handleLogin}
-            color={'secondary'}
+            color={"secondary"}
             label={"Login"}
             className="btn-ui"
           />
           <Link to="/signup">Not a user?</Link>
         </Paper>
       </div>
-    );
+    )
   }
 }
 
 const mapStateToProps = reduxState => {
-  const { wizard, id } = reduxState.reducer;
+  const { wizard, id } = reduxState.reducer
   return {
     wizard,
     id
-  };
-};
+  }
+}
 
 const mapDispatchToProps = {
   updateUser
-};
+}
 
 export default connect(
   mapStateToProps,
   mapDispatchToProps
-)(Login);
+)(Login)
